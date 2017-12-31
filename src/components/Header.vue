f99afa7e3b52d46d623fbf19ae8a81b985edb30d
Robbie Sherman <rob.w.sherman@gmail.com>
2017-12-31 14:26:17 +0000

    Make header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>The header component</h1>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+
+      <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link to="portfolio" activeClass="active" tag="li">
+            <a>Portfolio</a>
+          </router-link>
+          <router-link to="stocks" activeClass="active" tag="li">
+            <a>Stocks</a>
+          </router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="#">End day</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+              aria-expanded="false">
+              Save / Load
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Save Data</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Load Data</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
